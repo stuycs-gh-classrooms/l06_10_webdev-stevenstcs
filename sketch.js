7bd7e8d6d6f1c42babdb41c92cb064dab958aeb8
Steven Cen <76054479+stevenstcs@users.noreply.github.com>
2022-06-02 15:06:46 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
+var x;
+var y;
+var radius;
 function setup() {
-  // put setup code here
+  createCanvas(600,600);
+  background(155);
+  radius = 30;
+  x = (radius/2);
+  y = (radius/2);
 }
 
 function draw() {
-  // put drawing code here
+      background(155);
+        
+    if (x < 201){
+     fill(#FF0F0F);
+    }
+      else if (x < 401){
+       fill(#4AFF0F);
+      }
+        else{
+         fill(#0F31FF);
+        }
+    if (x > 600){
+       x = 0;
+       y = y + (radius*2);
+    }
+    circle(x, y, radius);
+    x = x + 1;
 }
